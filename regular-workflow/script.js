07a26cd12fc7c9aa7ea19d70c2297bbd30a680c4
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2022-02-04 20:13:57 -0500

    implemented dark/light mode toggling

--- regular-workflow/script.js
+++ regular-workflow/script.js
@@ -1,8 +1,12 @@
 let tasks = document.querySelectorAll('.task');
 
+let toggleBtn = document.querySelector('.toggle-mode');
+
 tasks.forEach((task) => {
     task.addEventListener('click', expandTask);
 })
+
+toggleBtn.addEventListener('click', toggleMode);
 
 function expandTask(e) {
     // console.log(e.currentTarget);
@@ -13,4 +17,19 @@ function expandTask(e) {
         e.currentTarget.classList.add('task-expand');
         e.currentTarget.querySelector('.task-details').style.visibility = 'visible';
     }
+}
+
+function toggleMode(e) {
+    let header = document.querySelector('header');
+    let left = document.querySelector('.left');
+    let right = document.querySelector('.right');
+    let tasks = document.querySelectorAll('.task-information');
+
+    e.currentTarget.classList.toggle('toggle-active');
+    header.classList.toggle('header-light');
+    left.classList.toggle('left-light');
+    right.classList.toggle('right-light');
+    tasks.forEach((task) => {
+        task.classList.toggle('task-light');
+    })
 }
